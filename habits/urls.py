bab54336d3896a292dd9a90501130654e19c0835
Artemii <aboga99@mail.ru>
2025-07-01 23:02:22 +0300

    Добавлен healthcheck endpoint /api/health/

--- habits/urls.py
+++ habits/urls.py
@@ -1,10 +1,16 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .views import HabitViewSet
+from django.http import JsonResponse
+
 
 router = DefaultRouter()
 router.register(r'habits', HabitViewSet, basename='habit')
 
+def health_check(request):
+    return JsonResponse({"status": "ok"})
+
 urlpatterns = [
+    path('health/', health_check),
     path('', include(router.urls)),
 ]
